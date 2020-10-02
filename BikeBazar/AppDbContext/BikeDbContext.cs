6fd9d2779443f64460d1b541ad67d9c720e59ce7
ashiq97 <ashiq070@gmail.com>
2020-10-02 16:21:17 +0600

    add Make and BikeModel

---
+++ BikeBazar/AppDbContext/BikeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BikeBazar.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeBazar.AppDbContext
+{
+    public class BikeDbContext : DbContext
+    {
+        public BikeDbContext(DbContextOptions<BikeDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<BikeModel> BikeModels { get; set; }
+    }
+}
---
+++ BikeBazar/Migrations/20201002101656_Added_Make_and_BikeModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BikeBazar.Migrations
+{
+    public partial class Added_Make_and_BikeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Makes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Makes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BikeModels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    MakeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BikeModels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BikeModels_Makes_MakeId",
+                        column: x => x.MakeId,
+                        principalTable: "Makes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BikeModels_MakeId",
+                table: "BikeModels",
+                column: "MakeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BikeModels");
+
+            migrationBuilder.DropTable(
+                name: "Makes");
+        }
+    }
+}
