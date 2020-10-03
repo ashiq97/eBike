151d53b1dd7afc36369e34b1f413fec483fb5def
ashiq97 <ashiq070@gmail.com>
2020-10-03 15:02:36 +0600

    CRUDE of Make entity and added font-awesome for styling button

--- BikeBazar/Controllers/MakeController.cs
+++ BikeBazar/Controllers/MakeController.cs
@@ -1,24 +1,76 @@
-﻿using BikeBazar.Models;
+﻿using BikeBazar.AppDbContext;
+using BikeBazar.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using System.Linq;
 
 namespace BikeBazar.Controllers
 {
     public class MakeController : Controller
     {
-        [Route("Make")]
-        [Route("Make/bikes")]
-        public IActionResult Bikes()
+        private readonly BikeDbContext _context;
+        public MakeController(BikeDbContext context)
         {
-            Make make = new Make { Id = 1, Name = "Jon doe" };
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var getAllData = _context.Makes.ToList();
+            return View(getAllData);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Make make)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Add(make);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }    
             return View(make);
         }
 
-        //attribute based routing
-        [Route("make/bikes/{year:int:length(4)}/{month:int:range(1,13)}")]
-        public IActionResult ByYearMonth(int year,int month)
+
+        [HttpPost]
+        public IActionResult Delete(int id)
         {
-            return Content(year + ";" + month);
+            var make = _context.Makes.Find(id);
+            if(make == null)
+            {
+                return NotFound();
+            }
+            _context.Makes.Remove(make);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
+
+        public IActionResult Edit(int id)
+        {
+            var make = _context.Makes.Find(id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            return View(make);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(make);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(make);
+        }
+
     }
 }
